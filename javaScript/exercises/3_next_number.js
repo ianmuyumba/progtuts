0fb2ea71b6efd70cc7e836bc14bc81e57537e47a
Ian Muyumba <ianmandela@gmail.com>
2020-05-20 00:10:16 +0300

    edabit exercises

---
+++ javaScript/exercises/3_next_number.js
@@ -0,0 +1,17 @@
+/*
+  Return the Next Number from the Integer Passed
+    - Create a function that takes a number as an argument,
+    increments the number by +1 and returns the result.
+*/
+
+// var myNum = 10;
+// myNum = myNum + 1;
+//
+// console.log(myNum);
+
+
+
+// Edabit solution
+function nextNum(num) {
+  return num + 1;
+}
