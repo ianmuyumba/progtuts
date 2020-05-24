c82424bf5912984caed8d284e0364876f0fbbb21
Ian Muyumba <ianmandela@gmail.com>
2020-05-24 14:29:03 +0300

    remainder

---
+++ javaScript/exercises/7_modulus.js
@@ -0,0 +1,14 @@
+/*
+  Return the Remainder from Two Numbers
+    - There is a single operator in JavaScript, capable of providing
+    the remainder of a division operation. Two numbers are passed as
+    parameters. The first parameter divided by the second parameter
+    will have a remainder, possibly zero. Return that value.
+*/
+var x = 2;
+var y = 1;
+
+function remainder(x, y) {
+	var myModulus = x % y;
+	return myModulus;
+}
