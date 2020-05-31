2c6a4e136a67befab2eff144a43057073983ad01
Ian Muyumba <ianmandela@gmail.com>
2020-06-01 00:40:37 +0300

    node basics
    Architecture
    How Node works
    sample program

---
+++ javaScript/nodejs/1 Getting Started/intro.js
@@ -0,0 +1,39 @@
+/*
+    What is Node
+      - Open source runtime environment for executing JavaScript Code outside
+      of a browser
+      - Quite often used to build backend services (API's - Web Apps & Mobile Apps)
+          * Store Data
+          * Send Emails
+          * Push Notifications
+      - Node is suitable for building highly scalable, data intensive & real time apps
+
+
+    What's so special about Node?
+      - Easy to get started and ideal for prototyping and agile development
+      - Ideal for building superfast and highly scalable web and mobile apps
+          * Uber, Netflix, PayPal have built their platforms on Node
+      - JavaScript everywhere - Both for front end and back end
+          * Codebases are thus cleaner and more consistent
+      - Large ecosystem of open-source libraries
+
+
+    Node Architecture
+      - Every browser has a JavaScript engine that converts JavaScript code into
+      machine language. The browser provides a runtime environment for JavaScript code
+          * Microsoft Edge - Chakra
+          * Mozilla Firefox - SpiderMonkey
+          * Google Chrome - v8 engine
+      - Node is a program that includes Chrome's v8 engine plus some additional
+      capabilities that cannot be found in the browser
+      - Node is not a programming language, neither is it a framework. It is a
+      runtime environment
+
+
+    How Node Works
+      - Asynchronous/Non-blocking architecture: several requests can be executed
+       without having to wait for one request to be accomplished
+      - Single thread handles all requests
+      - Not suitable for CPU intensive applications like video encoding or image
+      processing. Node is however suitable for data intensive applications
+*/
--- javaScript/nodejs/node/introduction.js
+++
@@ -1,6 +0,0 @@
-/*
-  Node.js - JavaScript Runtime Environment
-*/
-setTimeout(function() {
-  console.log("3 Seconds");
-}, 3000);
