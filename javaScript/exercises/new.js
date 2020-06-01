05c7b94f5f790c651ef63d4852bde7591eb88418
ianmuyumba <ianmandela@gmail.com>
2020-06-01 14:22:29 +0300

    uncomment code

--- javaScript/exercises/new.js
+++
@@ -1,7 +0,0 @@
-console.log("Convert minutes to seconds");
-
-var userMinutes = prompt("Enter minutes: ");
-
-var userSeconds = userMinutes * 60;
-
-console.log(`${userMinutes} Minutes = ${userSeconds} Seconds`);
--- javaScript/jstutorial/2_conditionalstatements/test.js
+++ javaScript/jstutorial/2_conditionalstatements/test.js
@@ -1,41 +1,41 @@
-// // /*
-// // 	Logical Operators
-// // 		- AND (&&)
-// // 		- OR (||)
-// // 		- NOT (!)
-// // */
+/*
+	Logical Operators
+		- AND (&&)
+		- OR (||)
+		- NOT (!)
+*/
 
-// // //Check if a number is greater than ten AND less than 20
+//Check if a number is greater than ten AND less than 20
 
-// // var number1 = prompt("Enter number: ")
+var number1 = prompt("Enter number: ")
 
-// // if ((number1 > 10) && (number1 < 20)) {
-// // 	console.log((number1 + " is greater than 10 AND less than 20"))
-// // } else {
-// // 	console.log((number1 + " is NOT a number between 10 and 20"))
-// // }
-
-
-// //Check if a number is greater than 10 OR less than 50
-
-// var number2 = prompt("Enter number: ")
-
-// if ((number2 > 10) || (number2 < 50)) {
-// 	console.log((number2 + " could be greater than 10 OR less than 20"))
-// } else {
-// 	console.log((number2 + " is NOT a number bwana"))
-// }
+if ((number1 > 10) && (number1 < 20)) {
+	console.log((number1 + " is greater than 10 AND less than 20"))
+} else {
+	console.log((number1 + " is NOT a number between 10 and 20"))
+}
 
 
-// //Check if a number is greater than 10 OR less than 50
+//Check if a number is greater than 10 OR less than 50
 
-// var number2 = prompt("Enter number: ")
+var number2 = prompt("Enter number: ")
 
-// if ((number2 > 10) || (number2 < 50)) {
-// 	console.log((number2 + " could be greater than 10 OR less than 20"))
-// } else {
-// 	console.log((number2 + " is NOT a number bwana"))
-// }
+if ((number2 > 10) || (number2 < 50)) {
+	console.log((number2 + " could be greater than 10 OR less than 20"))
+} else {
+	console.log((number2 + " is NOT a number bwana"))
+}
+
+
+//Check if a number is greater than 10 OR less than 50
+
+var number2 = prompt("Enter number: ")
+
+if ((number2 > 10) || (number2 < 50)) {
+	console.log((number2 + " could be greater than 10 OR less than 20"))
+} else {
+	console.log((number2 + " is NOT a number bwana"))
+}
 
 
 // The NOT (!) operator is used to negate an operation - True becomes false and vice versa
