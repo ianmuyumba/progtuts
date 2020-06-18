5748040c4cf9206082557fb59a2cd158f8321f6f
ianmuyumba <ianmandela@gmail.com>
2020-06-19 00:34:22 +0300

    test

---
+++ exercises/javascript/test.js
@@ -0,0 +1,21 @@
+/*
+	Edabit Test Exercises
+*/
+
+
+/*
+	Make a function using the && operator
+*/
+
+function andOperator() {
+	var a = 75;
+	// var b = 20
+
+	if ((a >= 70) && (a < 80)) {
+		console.log("Grade A-");
+	} else {
+		console.log("Grade ingine tu ya upuzi");
+	}
+}
+
+andOperator();
