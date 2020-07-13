99c7f906060fc39188f71d851a2c8a75c4f4b90e
Ian Muyumba <ianmandela@gmail.com>
2020-07-13 17:59:39 +0300

    added python files

--- javaScript/exercises/test.js
+++ javaScript/exercises/test.js
@@ -195,32 +195,49 @@
       monthName(12) ➞ "December"
       monthName(6) ➞ "June"
 */
+//
+// function monthName(num) {
+//   if (num === 1) {
+//     console.log("January");
+//   // } else if (num === 2) {
+//   //   return "February"
+//   // } else if (num === 3) {
+//   //   return "March"
+//   // }else if (num === 4) {
+//   //   return "April"
+//   // }else if (num === 5) {
+//   //   return "May"
+//   // }else if (num === 6) {
+//   //   return "June"
+//   // }else if (num === 7) {
+//   //   return "July"
+//   // }else if (num === 8) {
+//   //   return "August"
+//   // }else if (num === 9) {
+//   //   return "September"
+//   // }else if (num === 10) {
+//   //   return "October"
+//   // }else if (num === 11) {
+//   //   return "November"
+//   // }else if (num === 12) {
+//   //   return "December"
+//   }
+//
+// monthName(1)
 
-function monthName(num) {
-  if (num === 1) {
-    console.log("January");
-  // } else if (num === 2) {
-  //   return "February"
-  // } else if (num === 3) {
-  //   return "March"
-  // }else if (num === 4) {
-  //   return "April"
-  // }else if (num === 5) {
-  //   return "May"
-  // }else if (num === 6) {
-  //   return "June"
-  // }else if (num === 7) {
-  //   return "July"
-  // }else if (num === 8) {
-  //   return "August"
-  // }else if (num === 9) {
-  //   return "September"
-  // }else if (num === 10) {
-  //   return "October"
-  // }else if (num === 11) {
-  //   return "November"
-  // }else if (num === 12) {
-  //   return "December"
-  }
 
-monthName(1)
+
+/*
+    Return the Sum of Two Numbers
+      Create a function that takes two numbers as arguments and return their sum.
+
+      #Examples
+      addition(3, 2) ➞ 5
+      addition(-3, -6) ➞ -9
+      addition(7, 3) ➞ 10
+*/
+
+function addition(){
+  print("Ian");
+}
+addition()
